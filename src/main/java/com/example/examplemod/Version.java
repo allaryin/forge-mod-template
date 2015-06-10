ff3becef1c93168ea317f62485082e3bfb88731e
Ammon Lauritzen <allaryin@gmail.com>
2015-06-10 10:02:57 -0500

    Updated to support mod version in props file.

--- src/main/java/com/example/examplemod/Version.java
+++ src/main/java/com/example/examplemod/Version.java
@@ -3,7 +3,7 @@ package com.example.examplemod;
 public class Version {
 	public static final String	MOD_ID		= "ExampleMod";
 	public static final String	MOD_NAME	= "Example Mod";
-	public static final String	VERSION		= "0.0";
+	public static final String	VERSION		= "@VERSION@";
 
 	public static final String	CHANNEL		= MOD_ID;
 }
